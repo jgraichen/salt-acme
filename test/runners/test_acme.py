9c3c726c0e4b46e2048c7b6b73a7557f65fbd35c
Jan Graichen <jgraichen@altimos.de>
2020-01-22 17:49:04 +0100

    Add expectation test case for acme.sign runner
    The test case describe the most basic assumption on acme.sign, to sign a
    CSR and return a certificate. This not yet implemented.

--- test/runners/test_acme.py
+++ test/runners/test_acme.py
@@ -1,12 +1,30 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=missing-docstring
 
-def _read(path, mode='r'):
+from cryptography import x509
+from cryptography.x509 import oid
+from cryptography.hazmat.backends import default_backend
+
+
+def _read(path, mode="r"):
     with open(path, mode) as f:
         return f.read()
 
 
 def test_sign(runners):
-    ret = runners["acme.sign"](_read('test/fixtures/example.csr'))
+    ret = runners["acme.sign"](_read("test/fixtures/example.csr"))
 
-    assert ret['text'] == '----'
+    assert "text" in ret
+
+    crt = x509.load_pem_x509_certificate(ret["text"].encode(), default_backend())
+
+    cn = crt.subject.get_attributes_for_oid(oid.NameOID.COMMON_NAME)[0].value
+    san = crt.extensions.get_extension_for_class(x509.SubjectAlternativeName).value
+
+    assert cn == "example.org"
+    assert san == [
+        x509.DNSName("example.org"),
+        x509.DNSName("example.com"),
+        x509.DNSName("www.example.org"),
+        x509.DNSName("www.example.com"),
+    ]
