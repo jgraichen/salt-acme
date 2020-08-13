251119f02d490537a4dec8e9046ee9817e27602d
Jan Graichen <jgraichen@altimos.de>
2020-08-13 14:47:43 +0200

    Add wildcard test

--- test/modules/test_acme.py
+++ test/modules/test_acme.py
@@ -19,3 +19,17 @@ def test_sign(minion):
     )
 
     assert isinstance(crt, x509.Certificate)
+
+
+def test_sign_wildcard(minion):
+    with open("test/fixtures/example_wildcard.csr", "r") as f:
+        csr = f.read()
+
+    result = minion.mods["acme.sign"](csr)
+    assert "text" in result
+
+    crt = x509.load_pem_x509_certificate(
+        result["text"].encode(), backend=default_backend()
+    )
+
+    assert isinstance(crt, x509.Certificate)
